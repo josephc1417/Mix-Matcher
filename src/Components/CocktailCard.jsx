1a3a11ab3280176c0f3383e1b4570da4e3dad34c
Joseph Clay <Josephc1417@gmail.com>
2024-01-24 01:58:03 -0600

    Rendering CocktailCard

--- src/Components/CocktailCard.jsx
+++ src/Components/CocktailCard.jsx
@@ -1,10 +1,18 @@
+import {Link} from 'react-router-dom'
+import Wrapper from '../assets/wrappers/CocktailCard'
 
-
-const CocktailCard = () => {
+const CocktailCard = ({id,name,image,info,glass}) => {
   return (
-    <h2>
-      Cocktail Card
-    </h2>
+    <Wrapper>
+      <img src={image} alt={name} />
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn'>Details</Link>
+      </div>
+    
+    </Wrapper>
   )
 }
 
